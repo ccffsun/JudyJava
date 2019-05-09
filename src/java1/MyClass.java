9237c71cb72c56d9afa2d4a7e832d24631486c96
Judy Sun <ccffsun@gmail.com>
2019-05-08 23:51:15 -0700

    java data types practise

---
+++ src/java1/MyClass.java
@@ -0,0 +1,32 @@
+package java1;
+
+public class MyClass {
+    public static void main(String[] args){
+        String name= "John";
+        System.out.println(name);
+        int myNum=15;
+        System.out.println(myNum);
+        System.out.println("Hello "+name+"!");
+        String LastName="Doe";
+        String FirstName="John";
+        String FullName=FirstName+" "+LastName;
+        System.out.println(FullName);
+        int x=5;
+        int y=6;
+        System.out.println(x+y);//print the value of x+y
+        int a=4,b=5,c=7;
+        System.out.println(a+b+c);
+        byte myNums=100;
+        System.out.println(myNums);
+        short yourNum=5000;
+        System.out.println(yourNum);
+        long yourNums=800000;
+        System.out.println(yourNums);
+        float herNum=2.45f;
+        double herNums=4.89892;
+        System.out.println(herNum+herNums);
+
+
+
+    }
+}
