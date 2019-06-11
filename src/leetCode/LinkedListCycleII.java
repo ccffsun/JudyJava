ff6bd37e280f6dab49f09735cfa0b37068ecd5d2
Judy Sun <ccffsun@gmail.com>
2019-06-11 14:37:32 -0700

    update Linked List cycleII

---
+++ src/leetCode/LinkedListCycleII.java
@@ -0,0 +1,21 @@
+package leetCode;
+
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && slow != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+        }
+        return null;
+    }
+}
