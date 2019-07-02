a938fd1bfc3cb181bc3ab9e4c057c9e5842f3db7
Judy Sun <ccffsun@gmail.com>
2019-07-01 22:33:22 -0700

    lowest common ancest of a binary tree

---
+++ src/leetCode/LowestCommonAncestOfABinaryTree.java
@@ -0,0 +1,17 @@
+package leetCode;
+
+public class LowestCommonAncestOfABinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null){
+            return null;
+        }
+        else if(root==p||root==q){
+            return root;
+        }
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        if(left==null) return right;
+        else if (right==null) return left;
+        else return root;
+    }
+}
