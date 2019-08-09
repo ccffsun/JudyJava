3cb3fe7d2f617ab48bfe2d28e99d607a362e46ea
Judy Sun <ccffsun@gmail.com>
2019-08-09 16:09:41 -0700

    update flatten a binary tree

--- src/leetCode/FlattenABinaryTree.java
+++ src/leetCode/FlattenABinaryTree.java
@@ -1,6 +1,14 @@
 package leetCode;
 
+import dataStructure.Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class FlattenABinaryTree {
+
+    //recursion left first, right then
     public void flatten(TreeNode root) {
         if(root==null) return;
         TreeNode copyLeft = root.left;
@@ -22,14 +30,67 @@ public class FlattenABinaryTree {
             copyLeft.right=copyRight;
         }
     }
+
+    //recursion right first, left then
+    public void flattenRightFirst(TreeNode root){
+        if(root==null) return;
+        TreeNode tempRight = null;
+        if (root.right != null) {
+            flattenRightFirst(root.right);
+            tempRight=root.right;
+            root.right=null;//cut the right side from the root to avoid right side loop
+        }
+        if(root.left!=null) {
+            flattenRightFirst(root.left);
+            root.right=root.left;
+            root.left=null;
+            }
+        TreeNode curr=root;
+        while(curr.right!=null){
+          curr=curr.right;
+        }
+        curr.right=tempRight;
+    }
+
+
+
+
         private TreeNode prev;
     public void flattenII(TreeNode root){
         if(root==null) return;
-        flattenII(root.right);
-        flattenII(root.left);
-        root.right=prev;
+        flattenII(root.right);//this step prev=root.right ,root.right.left=null
+        flattenII(root.left);//this step root.left.right=root.right ,root.left.left=null ,prev=root.left
+        root.right=prev;//this step root.right=root.left
         root.left=null;
         prev=root;
+    }
+
+    public void flattenIII(TreeNode root){
+        if (root == null){
+            return;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while(stack.size()>0){
+            TreeNode curr = stack.pop();
+            if (curr.right!=null){
+                stack.push(curr.right);
+            }
+            if (curr.left!=null){
+                stack.push(curr.left);
+            }
+            curr.left = null;
+            queue.add(curr);
+        }
+
+        TreeNode curr = queue.poll();
+        while (queue.size()>0){
+            curr.right = queue.poll();
+            curr = curr.right;
+        }
     }
 }
 
---
+++ src/leetCode/IntervalListIntersection.java
@@ -0,0 +1,14 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntervalListIntersection {
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        if(A==null||B==null||A.length==0||B.length==0){
+            return new int[][]{};
+        }
+
+        List<int[]> interval= new ArrayList<>();
+    }
+}
