3ce721fcfde2f0136db4d7a6c0e4a99b383ae1fc
Judy Sun <ccffsun@gmail.com>
2019-08-12 16:38:06 -0700

    interval list intersection & partition list

--- src/leetCode/IntervalListIntersection.java
+++ src/leetCode/IntervalListIntersection.java
@@ -9,6 +9,20 @@ public class IntervalListIntersection {
             return new int[][]{};
         }
 
-        List<int[]> interval= new ArrayList<>();
+        List<int[]> ans= new ArrayList<>();
+        int i=0,j=0;
+        while(i<A.length && j<B.length){
+            int start=Math.max(A[i][0],B[j][0]);
+            int end=Math.min(A[i][1],B[j][1]);
+            if(start<=end) ans.add(new int[]{start,end});
+            if(A[i][1]>B[j][1]) j++;
+            else i++;
+        }
+        int [][] res=new int[ans.size()][2];
+        i=0;
+        for(int []pair :ans){
+            res[i++]=pair;
+        }
+        return res;
     }
 }
--- src/leetCode/PartitionList.java
+++ src/leetCode/PartitionList.java
@@ -29,4 +29,23 @@ public class PartitionList {
      greater.next=null;
      return dummy.next;
     }
+
+    public ListNode partitionII(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(0), dummy2 = new ListNode(0);
+        ListNode curr1 = dummy1, curr2 = dummy2;
+        while (head != null) {
+            if (head.val < x) {
+                curr1.next = head;
+                curr1 = head;
+            } else {
+                curr2.next = head;
+                curr2 = head;
+            }
+            head = head.next;
+        }
+
+        curr2.next=null;
+        curr1.next=dummy2.next;
+        return dummy1.next;
+    }
 }
