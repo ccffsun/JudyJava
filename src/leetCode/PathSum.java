40297376d43401c6392cc99e1e4abfc1591f9bcb
Judy Sun <ccffsun@gmail.com>
2019-08-22 15:04:30 -0700

    path sum non recursive

--- src/leetCode/PathSum.java
+++ src/leetCode/PathSum.java
@@ -17,11 +17,46 @@ package leetCode;
 // /  \      \
 //7    2      1
 //return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+
+import dataStructure.Tree;
+
+import java.util.Stack;
+
+//recursive    sum - root.val
 public class PathSum {
     public boolean hasPathSum(TreeNode root, int sum) {
      if(root==null) return false;
      if(root.left==null&&root.right==null) return root.val==sum;
      return hasPathSum(root.left,sum-root.val)||hasPathSum(root.right,sum-root.val);
+    }
+
+
+    public boolean hasPathSumII(TreeNode root, int sum){
+        Stack<TreeNode> stack=new Stack<>();
+        Stack<Integer> sums=new Stack<>();
+
+        stack.push(root);
+        sums.push(sum);
+
+        while(stack.size()!=0 && root!=null){
+            TreeNode top= stack.pop();
+            int value= sums.pop();
+
+            if(top.left==null&&top.right==null&&top.val==value){
+                return true;
+            }
+            if(top.left!=null){
+                stack.push(top.left);
+                sums.push(value-top.val);
+            }
+            if(top.right!=null){
+                stack.push(top.right);
+                sums.push(value-top.val);
+            }
+        }
+        return false; //出口为 if(top.left==null&&top.right==null&&top.val==value) return true;
+
     }
 }
 
