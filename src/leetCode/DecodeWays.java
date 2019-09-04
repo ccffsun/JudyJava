19772b6a14a4079a1b605b173099673fa68275fe
Judy Sun <ccffsun@gmail.com>
2019-09-03 17:19:38 -0700

    decodeways

---
+++ src/leetCode/DecodeWays.java
@@ -0,0 +1,28 @@
+package leetCode;
+
+public class DecodeWays {
+        public int numDecodings(String s) {
+
+        if(s==null||s.length()==0){
+            return 0;
+        }
+        int l=s.length();
+        int[] dp=new int[l+1];
+        dp[0]=1;
+        dp[1]=s.charAt(0)!='0'?1:0;
+        char[]sc=s.toCharArray();
+
+        for(int i=2;i<=l;i++){
+            int first=Integer.valueOf(s.substring(i-1,i));
+            int second=Integer.valueOf(s.substring(i-i,i));
+            if(first >= 1 && first <= 9) {
+                dp[i] += dp[i-1];
+            }
+            if(second >= 10 && second <= 26) {
+                dp[i] += dp[i-2];
+                }
+            }
+
+        return dp[l];
+    }
+}
