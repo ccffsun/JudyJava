43cb431769413ecf245dc1b17549d3922f3466f9
Judy Sun <ccffsun@gmail.com>
2019-12-09 12:39:36 -0800

    swapNodesInParis recursion

--- src/leetCode/SwapNodesInPairs.java
+++ src/leetCode/SwapNodesInPairs.java
@@ -18,4 +18,17 @@ public class SwapNodesInPairs {
         prev.next=curr;
         return dummy.next;
     }
+
+    //recursion
+
+    public ListNode swapPairsII(ListNode head){
+        if(head==null||head.next==null){
+            return head;// 1.connect the prevs to the head 2. terminate the recursion
+        }
+        ListNode second=head.next;
+        ListNode temp=head.next.next;// compare temp=second.next?
+        head.next=swapPairsII(temp);
+        second.next=head;
+        return second;
+    }
 }
