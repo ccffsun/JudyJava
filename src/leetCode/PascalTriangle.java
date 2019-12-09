51caf24653dafda4aa848e88c028abc2d089f4b1
Judy Sun <ccffsun@gmail.com>
2019-12-09 15:38:19 -0800

    pascal triangleII

--- src/leetCode/PascalTriangle.java
+++ src/leetCode/PascalTriangle.java
@@ -17,4 +17,18 @@ public class PascalTriangle {
         }
         return result;
     }
+
+    public List<Integer> getRow(int rowIndex) {
+
+        ArrayList<Integer> prev=null;
+        for(int i=1;i<=rowIndex+1;i++){
+            ArrayList<Integer> save= new ArrayList<Integer>();
+            for(int j=1;j<=i;j++){
+                if(j==1||j==i) save.add(1);
+                else save.add(prev.get(j-1)+prev.get(j-2));
+            }
+            prev=save;
+        }
+        return prev;
+    }
 }
